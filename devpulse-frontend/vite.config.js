bc3319c5597d04fa41984a6f67152645d13c3263
Karan Kaushal <karan-email@gmail.com>
2026-09-03 21:57:11 +0530

    Initial commit for Devpulse frontend and backend

---
+++ devpulse-frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  define: {
+    'process.env.REACT_APP_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || 'http://localhost:5000')
+  },
+  base: '/DevPulse/',
+})
